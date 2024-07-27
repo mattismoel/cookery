ac97dd30b09ebcd588b7115390d17325f2485055
Mattis Kristensen <mattismoel@gmail.com>
2024-07-27 19:08:20 +0200

    add register page

---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Button from "$lib/component/Button.svelte";
+  import InputText from "$lib/component/InputText.svelte";
+</script>
+
+<main class="mx-responsive flex items-center h-[calc(100vh-var(--nav-height))]">
+  <form class="w-full flex flex-col gap-2" method="post">
+    <h1 class="text-2xl font-semibold mb-4">Join the cookery.</h1>
+    <InputText
+      props={{ type: "email", placeholder: "Email", name: "email" }}
+      align="center"
+    />
+    <InputText
+      props={{ type: "password", placeholder: "Password", name: "password" }}
+      align="center"
+    />
+    <InputText
+      props={{
+        type: "password",
+        placeholder: "Repeat Password",
+        name: "passwordRepeat",
+      }}
+      align="center"
+    />
+    <Button props={{ type: "submit" }} fitX="expand">Register</Button>
+    <div class="text-gray-500">
+      Already have an account? <a class="font-semibold" href="/login"
+        >Login here.</a
+      >
+    </div>
+  </form>
+</main>
