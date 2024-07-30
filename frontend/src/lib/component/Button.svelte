165d4738a63761c3faff8d43244667bb75f4f727
Mattis Kristensen <mattismoel@gmail.com>
2024-07-30 15:58:15 +0200

    update button styling

--- frontend/src/lib/component/Button.svelte
+++ frontend/src/lib/component/Button.svelte
@@ -3,8 +3,10 @@
   import type { HTMLButtonAttributes } from "svelte/elements";
 
   type Fit = "shrink" | "expand";
+  type Type = "primary" | "secondary" | "danger";
 
   export let fitX: Fit = "shrink";
+  export let type: Type = "primary";
   export let props: HTMLButtonAttributes;
 
   const dispatch = createEventDispatcher();
@@ -17,10 +19,21 @@
     ["shrink", ""],
     ["expand", "w-full"],
   ]);
+
+  const typeMap = new Map<Type, string>([
+    [
+      "primary",
+      "bg-green-400 hover:bg-green-500 border-green-500 hover:border-green-600 text-white fill-white",
+    ],
+    [
+      "danger",
+      "bg-red-400 hover:bg-red-500 border-red-500 hover:border-red-600 text-red-50 fill-red-50",
+    ],
+  ]);
 </script>
 
 <button
-  class={`bg-green-400 hover:bg-green-500 border-[1px] border-green-500 hover:border-green-600 font-semibold text-white py-2 px-4 rounded-sm ${fitMap.get(fitX)}`}
+  class={`border-[1px] font-semibold py-2 px-4 rounded-sm flex items-center justify-center gap-2 ${fitMap.get(fitX)} ${typeMap.get(type)}`}
   on:click={onClick}
   {...props}
 >
