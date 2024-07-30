6329f4972cbc291c52d4d27262fcf02e5ecc1b4f
Mattis Kristensen <mattismoel@gmail.com>
2024-07-30 15:56:35 +0200

    update icon implementation

---
+++ frontend/src/lib/assets/icons/Icon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let size: number = 24;
+  export let extraClasses: string = "";
+
+  $: sizePx = `${size}px`;
+</script>
+
+<svg
+  class={extraClasses}
+  style:width={sizePx}
+  style:height={sizePx}
+  viewBox={`0 0 24 24`}
+>
+  <slot />
+</svg>
